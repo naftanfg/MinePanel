f2ebebfd591d59a1b39b96d49dc508faa223adba
naftanfg <118559497+naftanfg@users.noreply.github.com>
2025-04-12 23:26:23 +0200

    Versione Italiana

--- Versione Italiana/app.py
+++ Versione Italiana/app.py
@@ -140,4 +140,4 @@ def elimina(versione):
 if __name__ == "__main__":
     init_db()
     os.makedirs("server-file", exist_ok=True)
-    app.run(debug=True, port=5070)
+    app.run(debug=False, port=5070)
