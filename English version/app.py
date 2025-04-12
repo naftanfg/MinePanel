1da76b7e2b2e3d71b63852c00a54e349aeffd581
naftanfg <118559497+naftanfg@users.noreply.github.com>
2025-04-12 23:27:17 +0200

    English version

--- English version/app.py
+++ English version/app.py
@@ -140,4 +140,4 @@ def delete(version):
 if __name__ == "__main__":
     init_db()
     os.makedirs("server-files", exist_ok=True)
-    app.run(debug=True, port=5060)
+    app.run(debug=False, port=5060)
